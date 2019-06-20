a806374e4df0f420c7f6d47b69a793454167b32a
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-06-20 15:19:38 -0500

    WIP: TabLink Class Constructor Complete.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,25 +1,31 @@
 
 class TabLink {
   constructor(element) {
+    
     // Assign this.element to the passed in DOM element
-    // this.element;
-    
+    this.element = element;
+    // console.log(this.element); 
+
     // Get the custom data attribute on the Link
-    // this.data;
-    
+    this.data = element.dataset.tab;
+    // console.log(this.data); 
+
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`)
+    // console.log(this.itemElement); 
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
+    // console.log(this.tabItem); 
     
     // Add a click event listener on this instance, calling the select method on click
-
+    element.addEventListener("click", () => this.select())
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll(".tabs-link");
+    console.log(links);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
@@ -35,12 +41,12 @@ class TabLink {
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element = element;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    // const items = ;
 
     // Remove the class "tabs-item-selected" from each element
     
@@ -59,4 +65,10 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+// all classes named ".tabs-link"
+links = document.querySelectorAll(".tabs-link");
+
+// returns new instances of TabLink
+links.forEach((link) => {return new TabLink(link)})
+
+console.log(links); 
